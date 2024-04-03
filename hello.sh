6201beb5490eaa85eafd38179b79aae0c6488f16
kriricha <90825381+kriricha@users.noreply.github.com>
2024-04-03 11:08:06 +0530

    first commit

---
+++ hello.sh
@@ -0,0 +1,36 @@
+! /bin/bash
+echo "Hello World"
+echo "jam jam jambura jaadu tuu sik le pura"
+# # echo Our shell name is $BASH_VERSION
+# # echo the version of shell is $HOME
+# # echo the current working directory is$PWD
+
+# # name=RichieRich
+# # val=10
+# # echo The Name is $name
+# # echo value $val
+# # echo "Enter names:"
+# # read name1 name2 name3
+# # echo "Entered name :$name1,$name2,$name3"
+# # read -p 'username:' user_var
+# # echo "username: $user_var"
+# # read -sp 'password :' pass_var
+# # echo "password: $pass_var"
+# # // TO TAKE INPUT & SHOW OUTPUT IN ARRAY FORM
+# # echo "Enter Numbers:"
+# # read -a names
+# # echo "Names : ${names[0]}, ${names[1]}"
+# # //WITHOUT PROVIDING VARIABLE NAME automatically get stored in a reply
+# # echo "Enter name :"
+# # read
+# # echo "Name : $REPLY"
+# #  for passing argument in a script & printing it here it will print 3 args
+# # echo  $1 $2 $3
+# # by passing argument as a array
+# args=("$@")
+# # echo ${args[o]} ${args[1]} ${args[2]} ${args[3]}
+# # to print all the args at once
+# echo $@
+# # also we can pass as many args at a time
+# # it says no. of args get passed
+# echo $#
